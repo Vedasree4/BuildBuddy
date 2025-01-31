13b14751ed2e07b879bc6b28f65221dbc3ddc195
Shreya-2407 <129676581+Shreya-2407@users.noreply.github.com>
2025-01-31 12:20:51 +0530

    Added about page file

---
+++ about.php
@@ -0,0 +1,15 @@
+
+
+<div class="col-12">
+    <div class="card">
+        <div class="card-body pt-4">
+            <div class="container-fluid page-content">
+                <!-- PHP block to dynamically include content from the 'about.html' file -->
+                <?php 
+                    if(is_file(base_app.'pages/about.html'))
+                    echo file_get_contents((base_app.'pages/about.html'));
+                ?>
+            </div>
+        </div>
+    </div>
+</div>
