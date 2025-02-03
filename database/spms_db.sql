62913847e79f6f2c69acb43600993520d581efaf
Shreya-2407 <129676581+Shreya-2407@users.noreply.github.com>
2025-02-03 20:13:24 +0530

    Update spms_db.sql

--- database/spms_db.sql
+++ database/spms_db.sql
@@ -18,32 +18,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `inquiry_list`
---
-
-CREATE TABLE `inquiry_list` (
-  `id` bigint(30) NOT NULL,
-  `fullname` text NOT NULL,
-  `contact` text NOT NULL,
-  `email` text NOT NULL,
-  `message` text NOT NULL,
-  `status` tinyint(1) NOT NULL DEFAULT 0,
-  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
-  `updated_at` datetime DEFAULT NULL ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `inquiry_list`
---
-
-INSERT INTO `inquiry_list` (`id`, `fullname`, `contact`, `email`, `message`, `status`, `created_at`, `updated_at`) VALUES
-(1, 'Mark Cooper', '09123456789', 'mcooper@mail.com', 'This is a sample inquiry only', 0, '2023-04-26 15:29:29', NULL),
-(2, 'Claire Blake', '091265478933', 'cblake@mail.com', 'Sample Message only', 0, '2023-04-26 15:30:16', NULL),
-(3, 'George Willson', '09654567788', 'gwilson@mail.com', 'In aliquam, arcu sed tincidunt porta, dui leo porta est, eget blandit leo ipsum nec leo. Sed faucibus faucibus ante eget auctor. Curabitur vitae justo pretium enim facilisis tempus vehicula sit amet augue. Maecenas viverra commodo pretium. Maecenas vitae augue sit amet lectus vestibulum facilisis. Quisque neque neque, interdum lacinia nulla nec, bibendum tincidunt leo.', 1, '2023-04-26 15:32:07', '2023-04-26 15:42:10');
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `service_list`
 --
 
@@ -105,40 +79,8 @@ INSERT INTO `system_info` (`id`, `meta_field`, `meta_value`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
 --
 
-CREATE TABLE `users` (
-  `id` int(50) NOT NULL,
-  `firstname` varchar(250) NOT NULL,
-  `middlename` text DEFAULT NULL,
-  `lastname` varchar(250) NOT NULL,
-  `username` text NOT NULL,
-  `password` text NOT NULL,
-  `avatar` text DEFAULT NULL,
-  `last_login` datetime DEFAULT NULL,
-  `type` tinyint(1) NOT NULL DEFAULT 0,
-  `date_added` datetime NOT NULL DEFAULT current_timestamp(),
-  `date_updated` datetime DEFAULT current_timestamp() ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='2';
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`id`, `firstname`, `middlename`, `lastname`, `username`, `password`, `avatar`, `last_login`, `type`, `date_added`, `date_updated`) VALUES
-(1, 'Adminstrator', '', 'Admin', 'admin', '$2y$10$lu9Lz9d61nsRRq5aXGOrmuik6tzhMif.AIQTmxgj4LTHf3M9hyGtW', 'uploads/avatars/1.png?v=1678760026', NULL, 1, '2021-01-20 14:02:37', '2023-04-26 16:01:02'),
-(9, 'Claire', '', 'Blake', 'cblake', '$2y$10$DFEet3AmXnsVKls912SbHey87bsXauL7nannya2CjtV7m37dNZhNe', 'uploads/avatars/9.png?v=1682495668', NULL, 2, '2023-04-26 15:54:27', '2023-04-26 16:02:36');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `inquiry_list`
---
-ALTER TABLE `inquiry_list`
-  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `service_list`
@@ -153,22 +95,13 @@ ALTER TABLE `system_info`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `inquiry_list`
---
-ALTER TABLE `inquiry_list`
-  MODIFY `id` bigint(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
---
 -- AUTO_INCREMENT for table `service_list`
 --
 ALTER TABLE `service_list`
@@ -180,9 +113,4 @@ ALTER TABLE `service_list`
 ALTER TABLE `system_info`
   MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
-COMMIT;
+
